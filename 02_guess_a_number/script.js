82b7e919028d5065eacf7dbbf82bbae189b11346
Fullpotatoes <ludovic.pouet@gmail.com>
2024-10-25 16:03:35 +0200

    Step 3

--- 02_guess_a_number/script.js
+++ 02_guess_a_number/script.js
@@ -1,16 +1,27 @@
-function numbPrompt() {
+function askNumber() {
     let givenNumber = Number(prompt('saisissez un nombre'));
     return givenNumber ;
 }
 
 function didWin(givenNumber) {
     if (givenNumber > 22) {
-        alert("Plus grand");
-    } else if (givenNumber < 22) {
         alert("Plus petit");
+        return false;
+    } else if (givenNumber < 22) {
+        alert("Plus grand");
+        return false;
     } else {
         alert("Bravo ! Vous avez deviné le nombre");
+        return true;
     }
 }
 
-console.log(didWin(numbPrompt()));
+function gameplay() {
+    let win = false;
+    while (!win) {
+        const givenNumber = askNumber();
+        win = didWin(givenNumber);
+    }
+}
+
+gameplay();
