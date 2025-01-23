ebc61affafb90cb6924b6de8eda5451188f6474f
Fullpotatoes <ludovic.pouet@gmail.com>
2025-01-23 11:26:47 +0100

    Recursive end

--- 06_récursivité/recurs.js
+++ 06_récursivité/recurs.js
@@ -26,7 +26,7 @@ const dossierPrincipal = {
         contenu: [{
           nom: 'Portfolio',
           contenu: [{
-              nom: 'index.html'
+            nom: 'index.html'
             },
             {
               nom: 'script.js'
@@ -37,38 +37,46 @@ const dossierPrincipal = {
     ],
   }
 
-
 // Etape 1:
+  
+  
+  const afficherDossier = (dossier) => {
+      console.log(dossier.nom)
+  }
+  
+  afficherDossier(dossierPrincipal)
+  
 
+// Etape 2/3:
 
-// const afficherDossier = (dossier) => {
-//     console.log(dossier.nom)
-// }
-
-// afficherDossier(dossierPrincipal)
-
-
-// Etape 2:
-
-
-const afficherDossierIteratif = (dossier) => {
+  const afficherDossierIteratif = (dossier) => {
     console.log(dossier.nom)
     for (const item of dossier.contenu) {
-      console.log(item.nom)
-    }
-}
+      console.log(item.nom);
+      if (item.contenu) {
+        for (const sousItem of item.contenu) {
+          console.log(sousItem.nom);
+          if (sousItem.contenu) {
+                  for (const metaItem of sousItem.contenu) {
+                      console.log(metaItem.nom);
+                  }
+              }
+          }
+      }
+  }
+};
 
 afficherDossierIteratif(dossierPrincipal)
 
 
-// const afficherDossierRecursif = (dossier) => {
-//   console.log(dossier.nom)
-//   if (dossier.contenu === undefined) {
-//     return;
-//   }
-//   for (const item of dossier.contenu) {
-//     afficherDossierRecursif(item)
-//   }
-// };
+const afficherDossierRecursif = (dossier) => {
+  console.log(dossier.nom)
+  if (dossier.contenu === undefined) {
+    return;
+  }
+  for (const item of dossier.contenu) {
+    afficherDossierRecursif(item)
+  }
+};
 
-// afficherDossierRecursif(dossierPrincipal)
+afficherDossierRecursif(dossierPrincipal)
